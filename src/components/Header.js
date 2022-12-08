a10c5ffc383af4b1342504d6ae7b2f9ce8762914
Renan Storrer <storrerrenan@gmail.com>
2022-12-08 16:22:56 -0300

    REQ 2 - HEADER

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 class Header extends Component {
   render() {
+    const { email, expenses } = this.props;
+    let field = 0;
+    expenses.forEach((expense) => {
+      const conversão = Number(expense.exchangeRates[expense
+        .currency].ask) * Number(expense.value);
+      const conversãoFixa = conversão.toFixed(2);
+      field += Number(conversãoFixa);
+    });
+    if (field === 0) {
+      field = '0.00';
+    }
     return (
-      <div>Header</div>
+      <div>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">
+          {field}
+        </p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
