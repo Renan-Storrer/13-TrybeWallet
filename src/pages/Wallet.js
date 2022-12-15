7455abc5ee982ec68ac61f20f6e84802fd7577c5
Renan Storrer <storrerrenan@gmail.com>
2022-12-15 18:54:40 -0300

    mais erros lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import '../styles/wallet.css';
-
 // componentes
 import Header from '../components/Header';
 import Table from '../components/Table';
